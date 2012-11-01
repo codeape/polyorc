64756af5d641d96235f6bd471c6c356dd2a0e792
Oscar Norlander <oscar-no@codeape.org>
2012-11-01 11:16:11 +0100

    Cleaned up polyorcutils
    * Added documentation
    * Removed WSE

--- polyorclib/polyorcutils.c
+++ polyorclib/polyorcutils.c
@@ -3,14 +3,39 @@
 #include  <math.h>
 #include <stdlib.h>
 
+/**
+ * A int version of pow (see man pow).
+ *
+ * @author Oscar Norlander
+ *
+ * @param x
+ * @param y
+ *
+ * @return int
+ */
 int intpow(int x, int y) {
     return (int) rint(pow(x, y));
 }
 
+/**
+ * Frees a char ** but not its strings.
+ *
+ * @author Oscar Norlander
+ *
+ * @param arr The pointer to a char **.
+ */
 void free_array_of_charptr_excl(char ***arr) {
     free(*arr);
 }
 
+/**
+ * Frees a char ** and its strings.
+ *
+ * @author Oscar Norlander
+ *
+ * @param arr The pointer to a char **.
+ * @param len the number of strings.
+ */
 void free_array_of_charptr_incl(char ***arr, const int len) {
     int i;
     char **tmp = (*arr);
