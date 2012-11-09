5f348d80ac49b45faad29bedbf05c15be730dcc9
Oscar Norlander <oscar-no@codeape.org>
2012-11-09 17:07:23 +0100

    Added a common thing for output
    * I call it polyorcout
    * It support levels quiet, normal, verbose and debug
    * It supports color and no color output

---
+++ polyorclib/polyorcout.c
@@ -0,0 +1,214 @@
+/*
+ Originally created by Oscar Norlander (codeape)
+ ...................................................................
+ @@@@@@@    @@@@@@   @@@       @@@ @@@   @@@@@@   @@@@@@@    @@@@@@@
+ @@@@@@@@  @@@@@@@@  @@@       @@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@
+ @@!  @@@  @@!  @@@  @@!       @@! !@@  @@!  @@@  @@!  @@@  !@@
+ !@!  @!@  !@!  @!@  !@!       !@! @!!  !@!  @!@  !@!  @!@  !@!
+ @!@@!@!   @!@  !@!  @!!        !@!@!   @!@  !@!  @!@!!@!   !@!
+ !!@!!!    !@!  !!!  !!!         @!!!   !@!  !!!  !!@!@!    !!!
+ !!:       !!:  !!!  !!:         !!:    !!:  !!!  !!: :!!   :!!
+ :!:       :!:  !:!   :!:        :!:    :!:  !:!  :!:  !:!  :!:
+  ::       ::::: ::   :: ::::     ::    ::::: ::  ::   :::   ::: :::
+  :         : :  :   : :: : :     :      : :  :    :   : :   :: :: :
+ ...................................................................
+ Polyorc is under BSD 2-Clause License (see LICENSE file)
+*/
+
+#include "polyorcout.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#define RESETCOLOR "\033[0m"
+#define SELECTCOLOR "\033[%d;%d;m"
+
+static enum polyorc_verbosity orc_verbosity = orcm_normal;
+static enum polyorc_color orc_color = orcc_no_color;
+
+/**
+ * Sets the output verbosity level and if there will be colored
+ * output.
+ *
+ * @author Oscar Norlander
+ *
+ * @param v Verbosity level
+ * @param c Color mode
+ */
+void init_polyorcout(enum polyorc_verbosity v, enum polyorc_color c) {
+    orc_verbosity = v;
+    orc_color = c;
+}
+
+/**
+ * Returns the output level
+ *
+ * @author Oscar Norlander
+ *
+ * @return enum polyorc_verbosity
+ */
+enum polyorc_verbosity get_verbosity() {
+    return orc_verbosity;
+}
+
+/**
+ * Returns the color mode
+ *
+ * @author Oscar Norlander
+ *
+ * @return enum polyorc_color
+ */
+enum polyorc_color get_color() {
+    return orc_color;
+}
+
+/**
+ * Outputs a error message on stderr
+ *
+ * @author Oscar Norlander
+ *
+ * @param format The same as for printf
+ */
+void orcerror(const char *format, ...) {
+    va_list argptr;
+    printf("ERROR: ");
+    va_start(argptr, format);
+    vprintf(format, argptr);
+    va_end(argptr);
+}
+
+/**
+ * Prints a message if the verbosity level of the printout is
+ * equal or less than the level set with init_polyorcout
+ *
+ * @author Oscar Norlander
+ *
+ * @param verbosity Level
+ * @param format The same as for printf
+ */
+void orcout(enum polyorc_verbosity verbosity, const char *format, ...) {
+    if (verbosity <= orc_verbosity) {
+        va_list argptr;
+        va_start(argptr, format);
+        vprintf(format, argptr);
+        va_end(argptr);
+    }
+}
+
+/**
+ * Prints foreground colored output and reset the color
+ *
+ * @author Oscar Norlander
+ *
+ * @param attr Color attribute
+ * @param cv Color
+ * @param format The same as for printf
+ */
+void orcoutc(enum polyorc_color_attr attr, enum polyorc_color_val cv,
+             const char *format, ...) {
+    if (orcc_use_color ==  orc_color) {
+        printf(SELECTCOLOR, attr, cv);
+    }
+    va_list argptr;
+    va_start(argptr, format);
+    vprintf(format, argptr);
+    va_end(argptr);
+    if (orcc_use_color ==  orc_color) {
+        printf(RESETCOLOR);
+    }
+}
+
+/**
+ * Prints foreground colored output and inserts a new line
+ * character after reseting the color
+ *
+ * @author Oscar Norlander
+ *
+ * @param attr Color attribute
+ * @param cv Color
+ * @param format The same as for printf
+ */
+void orcoutcl(enum polyorc_color_attr attr, enum polyorc_color_val cv,
+              const char *format, ...) {
+    if (orcc_use_color ==  orc_color) {
+        printf(SELECTCOLOR, attr, cv);
+    }
+    va_list argptr;
+    va_start(argptr, format);
+    vprintf(format, argptr);
+    va_end(argptr);
+    if (orcc_use_color ==  orc_color) {
+        printf(RESETCOLOR);
+    }
+    printf("\n");
+}
+
+/**
+ * Prints a colored (if enabled) message and a formated message.
+ * If color is enabled a color (associated with the status code
+ * ) is put on the status message
+ *
+ * @author Oscar Norlander
+ *
+ * @param verbosity Level
+ * @param s Status code
+ * @param msg A status message
+ * @param format The same as for printf
+ */
+void status(enum polyorc_verbosity verbosity, enum polyorc_status s,
+            const char *msg, const char *format, ...) {
+    if (verbosity <= orc_verbosity) {
+        enum polyorc_color_val msg_color = orc_red;
+        switch (s) {
+        case orc_ok:
+            msg_color = orc_green;
+            break;
+        case orc_warn:
+            msg_color = orc_red;
+            break;
+        case orc_err:
+            break;
+        default:
+            break;
+        }
+        printf("[ ");
+        orcoutc(orc_reset, msg_color, msg);
+        printf(" ] ");
+        va_list argptr;
+        va_start(argptr, format);
+        vprintf(format, argptr);
+        va_end(argptr);
+    }
+}
+
+/**
+ * Something nice to look at
+ *
+ * @author Oscar Norlander
+ */
+void print_splash() {
+    orcoutcl(orc_reset, orc_red,
+        " ...................................................................");
+    orcoutcl(orc_reset, orc_green,
+        " @@@@@@@    @@@@@@   @@@       @@@ @@@   @@@@@@   @@@@@@@    @@@@@@@");
+    orcoutcl(orc_reset, orc_green,
+        " @@@@@@@@  @@@@@@@@  @@@       @@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@");
+    orcoutcl(orc_reset, orc_green,
+        " @@!  @@@  @@!  @@@  @@!       @@! !@@  @@!  @@@  @@!  @@@  !@@");
+    orcoutcl(orc_reset, orc_green,
+        " !@!  @!@  !@!  @!@  !@!       !@! @!!  !@!  @!@  !@!  @!@  !@!");
+    orcoutcl(orc_reset, orc_green,
+        " @!@@!@!   @!@  !@!  @!!        !@!@!   @!@  !@!  @!@!!@!   !@!");
+    orcoutcl(orc_bright, orc_green,
+        " !!@!!!    !@!  !!!  !!!         @!!!   !@!  !!!  !!@!@!    !!!");
+    orcoutcl(orc_bright, orc_green,
+        " !!:       !!:  !!!  !!:         !!:    !!:  !!!  !!: :!!   :!!");
+    orcoutcl(orc_bright, orc_green,
+        " :!:       :!:  !:!   :!:        :!:    :!:  !:!  :!:  !:!  :!:");
+    orcoutcl(orc_bright, orc_green,
+        "  ::       ::::: ::   :: ::::     ::    ::::: ::  ::   :::   ::: :::");
+    orcoutcl(orc_bright, orc_green,
+        "  :         : :  :   : :: : :     :      : :  :    :   : :   :: :: :");
+    orcoutcl(orc_reset, orc_red,
+        " ...................................................................");
+}
---
+++ polyorclib/polyorcout.h
@@ -0,0 +1,83 @@
+/*
+ Originally created by Oscar Norlander (codeape)
+ ...................................................................
+ @@@@@@@    @@@@@@   @@@       @@@ @@@   @@@@@@   @@@@@@@    @@@@@@@
+ @@@@@@@@  @@@@@@@@  @@@       @@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@
+ @@!  @@@  @@!  @@@  @@!       @@! !@@  @@!  @@@  @@!  @@@  !@@
+ !@!  @!@  !@!  @!@  !@!       !@! @!!  !@!  @!@  !@!  @!@  !@!
+ @!@@!@!   @!@  !@!  @!!        !@!@!   @!@  !@!  @!@!!@!   !@!
+ !!@!!!    !@!  !!!  !!!         @!!!   !@!  !!!  !!@!@!    !!!
+ !!:       !!:  !!!  !!:         !!:    !!:  !!!  !!: :!!   :!!
+ :!:       :!:  !:!   :!:        :!:    :!:  !:!  :!:  !:!  :!:
+  ::       ::::: ::   :: ::::     ::    ::::: ::  ::   :::   ::: :::
+  :         : :  :   : :: : :     :      : :  :    :   : :   :: :: :
+ ...................................................................
+ Polyorc is under BSD 2-Clause License (see LICENSE file)
+*/
+
+#ifndef POLYORCOUT_H
+#define POLYORCOUT_H
+
+enum polyorc_verbosity {
+    orcm_not_set = 0,
+    orcm_quiet,
+    orcm_normal,
+    orcm_verbose,
+    orcm_debug
+};
+
+enum polyorc_color {
+    orcc_not_set  = 0,
+    orcc_use_color,
+    orcc_no_color
+};
+
+enum polyorc_color_attr {
+    orc_reset = 0,
+    orc_bright = 1,
+    orc_dim = 2,
+    orc_underline = 3,
+    orc_blink = 5,
+    orc_reverse = 7,
+    orc_hidden = 8
+};
+
+
+enum polyorc_color_val {
+    orc_black = 30,
+    orc_red = 31,
+    orc_green = 32,
+    orc_yellow = 33,
+    orc_blue = 34,
+    orc_magenta = 35,
+    orc_cyan = 36,
+    orc_white =37
+};
+
+enum polyorc_status {
+    orc_ok,
+    orc_warn,
+    orc_err
+};
+
+void init_polyorcout(enum polyorc_verbosity, enum polyorc_color);
+
+void orcerror(const char* format, ...);
+
+enum polyorc_verbosity get_verbosity();
+
+enum polyorc_color get_color();
+
+void orcout(enum polyorc_verbosity, const char*, ...);
+
+void orcoutc(enum polyorc_color_attr, enum polyorc_color_val, const char*, ...);
+
+void orcoutcl(enum polyorc_color_attr, enum polyorc_color_val,
+              const char* format, ...);
+
+void status(enum polyorc_verbosity, enum polyorc_status, const char *,
+            const char*, ...);
+
+void print_splash();
+
+#endif
