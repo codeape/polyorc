aeab1f5b91b83fea794e041f1261bc83865d7897
Oscar Norlander <oscar-no@codeape.org>
2013-04-26 11:32:54 +0200

    Fixed printouts
    * Some printouts in the socket util was server specific.

--- polyorclib/polyorcsockutil.c
+++ polyorclib/polyorcsockutil.c
@@ -48,68 +48,67 @@ void init_socket(const int ipv, orc_socket_info *srv) {
 
 int create_socket(const char *ip, const int port, orc_socket_info *srv)
 {
+    orcoutcl(orc_reset, orc_blue, "Socket ip version %d", srv->ipv);
     if (4 == srv->ipv) {
-        orcoutcl(orc_reset, orc_blue, "Polyorc socket ip version 4");
         srv->fd = socket(PF_INET, SOCK_STREAM, 0);
         if (-1 == srv->fd) {
-            orcerror("Polyorc socket error %s (%d)\n", strerror(errno), errno);
+            orcerror("Socket error %s (%d)\n", strerror(errno), errno);
             return -1;
         }
         if (-1 == setnonblock(srv->fd)) {
-            orcerror("Polyorc socket error %s (%d)\n", strerror(errno), errno);
+            orcerror("Socket nonblock error %s (%d)\n", strerror(errno), errno);
             return -1;
         }
         srv->addr.addr4.sin_family = AF_INET;
         srv->addr.addr4.sin_port = htons(port);
         if (0 == ip) {
             srv->addr.addr4.sin_addr.s_addr = INADDR_ANY;
-            orcoutcl(orc_reset, orc_blue, "Polyorc socket ip ANY");
+            orcoutcl(orc_reset, orc_blue, "Socket ip ANY");
         } else {
             int ok = inet_pton(AF_INET, ip, &(srv->addr.addr4.sin_addr));
             if (0 == ok) {
-                orcerror("Polyorc socket address error for %s\n", ip);
+                orcerror("Socket address error for %s\n", ip);
                 return -1;
             } else if (0 > ok) {
-                orcerror("Polyorc socket address error %s (%d)\n", strerror(errno),
+                orcerror("Socket address error %s (%d)\n", strerror(errno),
                          errno);
                 return -1;
             }
-            orcoutcl(orc_reset, orc_blue, "Polyorc socket ip %s", ip);
+            orcoutcl(orc_reset, orc_blue, "Socket ip %s", ip);
         }
     } else if (6 == srv->ipv) {
-        orcoutcl(orc_reset, orc_blue, "Polyorc socket ip version 6");
         srv->fd = socket(PF_INET6, SOCK_STREAM, 0);
         if (-1 == srv->fd) {
-            orcerror("Polyorc socket error %s (%d)\n", strerror(errno), errno);
+            orcerror("Socket error %s (%d)\n", strerror(errno), errno);
             return -1;
         }
         if (-1 == setnonblock(srv->fd)) {
-            orcerror("Polyorc socket error %s (%d)\n", strerror(errno), errno);
+            orcerror("Socket nonblock error %s (%d)\n", strerror(errno), errno);
             return -1;
         }
         srv->addr.addr6.sin6_family = AF_INET6;
         srv->addr.addr6.sin6_port = htons(port);
         if (0 == ip) {
             srv->addr.addr6.sin6_addr = in6addr_any;
-            orcoutcl(orc_reset, orc_blue, "Polyorc socket ip ANY");
+            orcoutcl(orc_reset, orc_blue, "Socket ip ANY");
         } else {
             int ok = inet_pton(AF_INET6, ip, &(srv->addr.addr6.sin6_addr));
             if (0 == ok) {
-                orcerror("Polyorc socket address error for %s\n", ip);
+                orcerror("Socket address error for %s\n", ip);
                 free(socket);
                 return -1;
             } else if (0 > ok) {
-                orcerror("Polyorc socket address error %s (%d)\n", strerror(errno),
+                orcerror("Socket address error %s (%d)\n", strerror(errno),
                          errno);
                 free(socket);
                 return -1;
             }
-            orcoutcl(orc_reset, orc_blue, "Polyorc socket ip %s", ip);
+            orcoutcl(orc_reset, orc_blue, "Socket ip %s", ip);
         }
     } else {
-        orcerror("Internal: Uknown socket ip version %d\n", srv->ipv);
+        orcerror("Uknown socket ip version %d\n", srv->ipv);
         return -1;
     }
-    orcoutcl(orc_reset, orc_blue, "Polyorc socket port %d", port);
+    orcoutcl(orc_reset, orc_blue, "Socket port %d", port);
     return 0;
 }
